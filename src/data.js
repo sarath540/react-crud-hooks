5b2beafd60c7e7a1047d69f7f17e9abdb45bb586
Maddukuri Sarath <sarath540@gmail.com>
2021-06-11 11:10:33 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const userList = [
+    {
+        id: 0,
+        name: 'Sarath',
+        username: 'Sarath Bikkina'
+    },
+    {
+        id: 1,
+        name: 'Bharath',
+        username: 'Bharath Maddukuri'
+    }
+];
+
+export default userList;
